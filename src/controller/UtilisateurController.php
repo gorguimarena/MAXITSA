0e9e2321e0d77472f770cc8325889aa0ea412d08
gorguimarena <gorguimarena@gmail.com>
2025-07-11 20:23:07 +0000

    push apres faire un checkout sur jour3/us4

--- src/controller/UtilisateurController.php
+++ src/controller/UtilisateurController.php
@@ -1,23 +1,18 @@
 <?php
+
 namespace MAXITSA\CONTOLLER;
 
 use APP\CORE\ABSTRACT\AbstractController;
 
-class UtilisateurController extends AbstractController{
+class UtilisateurController extends AbstractController
+{
 
-    public function index() {
+    public function index()
+    {
         require_once '../templates/client/dashbord.php';
     }
-    public function show(){
-
-    }
-    public function create(){
-        
-    }
-    public function edit(){
-        
-    }
-    public function destroy(){
-
-    }
+    public function show() {}
+    public function create() {}
+    public function edit() {}
+    public function destroy() {}
 }
